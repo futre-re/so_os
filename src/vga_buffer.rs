16fccb74e59d74b5d14dc4d54d286ac134c3423f
no_to_last <77478360+futre-re@users.noreply.github.com>
2023-06-22 13:18:16 +0800

    修正bug

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -45,6 +45,7 @@ struct ScreenChar {
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
+#[repr(transparent)]
 struct Buffer {
     chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
